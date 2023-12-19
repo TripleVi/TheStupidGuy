035a265fbb77822774dff8eebf1d27a488608d2e
Vuong Vu <vuongvu061101@gmail.com>
2023-12-19 16:33:03 +0700

    updated interface

--- src/app.js
+++ src/app.js
@@ -2,23 +2,39 @@ import express from 'express';
 import session from 'express-session';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import MongoDBStore from "connect-mongodb-session";
 
 const app = express()
+
+const mongoStore = MongoDBStore(session);
+
+const store = new mongoStore({
+  collection: "userSessions",
+  uri: process.env.mongoURI,
+  expires: 1000,
+});
+
+// app.set("trust proxy", 1);
 
 app.use(cors({
     origin: 'https://triplevi.github.io',
     optionsSuccessStatus: 200,
     credentials: true,
+    exposedHeaders: ['set-cookie'],
 }))
 
 app.use(session({
+    name: 'access_token',
     secret: 'session secret',
+    store: store,
     resave: false,
     saveUninitialized: false,
     cookie: {
         secure: false,
-        httpOnly: true,
+        httpOnly: false,
         maxAge: 60000,
+        sameSite: 'none',
+        domain: 'localhost'
     },
 }))
 
@@ -31,16 +47,22 @@ app.get('/api/v1/test', (req, res) => {
     }
     console.log(req.session)
     console.log(req.sessionID)
-    // res.setHeader('')
+    // res.cookie('access_token', req.session.cookie, {
+    //     secure: false,
+    //     httpOnly: false,
+    //     maxAge: 60000,
+    //     sameSite: 'none',
+    // })
     res.send({
-        'data': 'hello world'
+        'id': req.session.id
     })
 })
 
 app.post('/api/v1/test', (req, res) => {
+    console.log(req.headers['cookie'])
+    console.log(req.headers['set-cookie'])
     console.log(req.session)
     console.log(req.session.id)
-    console.log(req.sessionID)
     res.send({
         data: 'hello world'
     })
