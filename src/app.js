86fdac9cb90e0cf06bb0b5f65a8e81d26da5c108
Vuong Vu <vuongvu061101@gmail.com>
2023-12-19 17:36:42 +0700

    updated session config

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: true,
-        httpOnly: false,
+        httpOnly: true,
         maxAge: 60000,
         sameSite: 'none',
     },
